6ca0409466c4f93b802845876abf5e797f3e3d17
Francesca Bonato <francesca.bonato@hotmail.com>
2024-03-30 16:01:01 +0100

    add es24-26 completed

--- es19-controlled-forms/react-project/src/components/Login.jsx
+++ es19-controlled-forms/react-project/src/components/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /*
 Add a "login" button to the Login component. This button should be disabled as long as the username and password inputs are empty.
 When clicked, the event handler attached to the button should call an onLogin function passed as a prop to the Login component, 
