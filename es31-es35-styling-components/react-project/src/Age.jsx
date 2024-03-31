8992e33fb66f2e8d36fb15b0040c83445c495587
Francesca Bonato <francesca.bonato@hotmail.com>
2024-03-31 18:42:43 +0200

    add es31 completed

---
+++ es31-es35-styling-components/react-project/src/Age.jsx
@@ -0,0 +1,6 @@
+/* eslint-disable react/prop-types */
+function Age({age}) {
+    return <p>Your age is {age}.</p>
+}
+
+export default Age;
--- es31-es35-styling-components/react-project/src/Welcome.jsx
+++ es31-es35-styling-components/react-project/src/Welcome.jsx
@@ -1,16 +1,12 @@
 /* eslint-disable react/prop-types */
-
-/*Starting from the last props exercise, modify the Age component so that the "Your age is " message is rendered only 
-if the age prop is greater than 18. Otherwise render the "You are very young!" message. */
-
 import Age from "./Age";
 
 function Welcome({ name, age }) {
   return (
-    <>
+    <div className="welcome">
       <p>Welcome, {name}!</p>
       {age>18? <Age age={age}/> : 'You are very young!'}
-    </>
+    </div>
   );
 }
 
